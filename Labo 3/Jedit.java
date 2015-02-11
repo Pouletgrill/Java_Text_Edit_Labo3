21df3549dc1da998d8ef0f765205af7d2eb4f0ec
PouletGrill <xavierb.2006@hotmail.com>
2015-02-11 14:49:17 -0500

    fefwe

--- Labo 3/Jedit.java
+++ Labo 3/Jedit.java
@@ -4,13 +4,13 @@ public class Jedit
 {
    public static void main ( String args[] )
    {
-      try 
+      try
       {
          if(args.length <= 0)
          {
             System.out.println("Aucun parametre:");
-            System.exit(1);            
-         }         
+            System.exit(1);
+         }
          else if (args[0].equals("-n") && args.length == 2)
          {
             EcrireTxt(args[1],true);
@@ -22,56 +22,72 @@ public class Jedit
       }
       catch( NumberFormatException e )
       {
-         System.err.println("Erreur ?!?!?!??!");
+         System.err.println("Parametre invalide");
          System.exit(1);
       }
-   }  
- 
-   
+   }
+
+
    static void EcrireTxt(String NomFichier, boolean Numero)
    {
       PrintWriter writer;
-      boolean pasFini = true;            
-      BufferedReader reader;      
+      boolean pasFini = true;
+      BufferedReader reader;
       reader = new BufferedReader(
                   new InputStreamReader( System.in));
-      if(NomFichier.exists())
+
+      File fichier = new File( NomFichier );
+     
+      if(fichier.exists())
       {
-         System.out.println("Le fichier """+NomFichier+""" existe déja")
-         System.out.println("Voulez-vous le remplacer?  O(oui) N(non)");
+         try
+         {
+            System.out.println("Le fichier "+NomFichier+" existe deja");
+            System.out.println("Voulez-vous le remplacer?  O(oui) N(non)");
+            String Reponse = reader.readLine();
+            if (Reponse .equals("N") || Reponse.equals("n"))
+            {
+               System.exit(1);
+               System.out.println("Exit ? ou pas...");
+            }
+         }
+         catch (IOException ex)
+         {
+            System.out.println(ex);
+         }
       }
-      
+
       try
       {
          writer = new PrintWriter(
                      new BufferedWriter(
-                        new FileWriter(NomFichier) ) );                        
-                   
-                   
+                        new FileWriter(NomFichier) ) );
+
+
          for(int i=1;pasFini;i++)
          {
-           String text =reader.readLine();          
+           String text =reader.readLine();
            if (text.length()>0)
-           {                     
+           {
                 if(Numero)
                 {
                  writer.println(i+": "+text);
-                 
+
                 }
                 else
                 {
-                 writer.println(text); 
+                 writer.println(text);
                 }
-                            
+
            }
            else
-           {              
-               pasFini=false;              
+           {
+               pasFini=false;
            }
          }
-         
+
          writer.close();
-         
+
          System.exit(1);
       }
       catch(IOException ioe)
@@ -79,6 +95,6 @@ public class Jedit
        System.err.println("Le fichier spécifié est introuvable");
        System.exit(1);
       }
-      
+
    }
 }
